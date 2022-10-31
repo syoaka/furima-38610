257c85e33de8d653fabcd10e726132d49fce95f1
syoaka <akaakaaka.com@gmail.com>
2022-10-31 14:07:02 +0900

    ActiveHashモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :pay_for_shipping
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :pay_for_shipping_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+  end
+
+  
+  validates :category_id,         numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :pay_for_shipping_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id,     numericality: { other_than: 1 , message: "can't be blank"}
 end
