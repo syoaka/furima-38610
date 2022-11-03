0c893e00aaa869f615852d5037b838ebf4c90129
syoaka <akaakaaka.com@gmail.com>
2022-11-03 17:38:32 +0900

    updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:show, :edit, :show]
+  before_action :set_tweet, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
