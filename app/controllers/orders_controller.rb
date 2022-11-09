228c38d8d43a586d7dce85ade774b61666e70b89
syoaka <akaakaaka.com@gmail.com>
2022-11-09 17:16:19 +0900

    購入表示修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
+    end 
   end
 
   def create
