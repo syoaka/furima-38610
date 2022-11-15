f1a1f80263cabd66fc26baf4d48fa9fc2cb3eb3a
Syota Akasaka <akaakaaka.com@gmail.com>
2022-11-15 15:44:21 +0900

    deploy.rb追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-38610'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'syoaka/furima-38610.git'
+set :repo_url,  'git@github.com:syoaka/furima-38610.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
